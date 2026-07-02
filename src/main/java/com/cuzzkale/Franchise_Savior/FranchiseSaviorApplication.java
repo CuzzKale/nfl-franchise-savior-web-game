0556d2c808111ddce6f4ec26bbe84d17bfe4a216
“CuzzKale” <kyle244kyle@outlook.com>
2026-07-02 14:09:26 -0500

    Initial Commit

---
+++ src/main/java/com/cuzzkale/Franchise_Savior/FranchiseSaviorApplication.java
@@ -0,0 +1,14 @@
+package com.cuzzkale.Franchise_Savior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranchiseSaviorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranchiseSaviorApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/com/cuzzkale/Franchise_Savior/FranchiseSaviorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuzzkale.Franchise_Savior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranchiseSaviorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
