84436ef3ce653ba2e02147d14c7572e42bab051a
“CuzzKale” <kyle244kyle@outlook.com>
2026-07-02 14:40:54 -0500

    added SQL database

--- src/main/java/com/cuzzkale/Franchise_Savior/FranchiseSaviorApplication.java
+++ src/main/java/com/cuzzkale/Franchise_Savior/FranchiseSaviorApplication.java
@@ -8,7 +8,7 @@ public class FranchiseSaviorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FranchiseSaviorApplication.class, args);
-		System.out.println("Hello World");
+
 	}
 
 }
